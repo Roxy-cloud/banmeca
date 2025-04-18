6e36be76cf05d11251bae9f4e58e8f0f1e4b554f
Roxy-Cloud <rosanagarrido1972@gmail.com>
2025-04-18 12:53:48 -0400

    Corrigiendo conflictos en app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,24 +3,16 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Route;
-=======
->>>>>>> e2a8b4e (Primer commit)
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-<<<<<<< HEAD
         api: __DIR__.'/../routes/api.php',
-=======
->>>>>>> e2a8b4e (Primer commit)
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-<<<<<<< HEAD
-        // Add global middleware (applied to all requests)
         $middleware->prepend([
             \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
             \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
@@ -28,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         ]);
 
-        //Define API middleware
+        // Define API middleware
         $apiMiddleware = [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
@@ -37,7 +29,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         // Apply API middleware to the API routes
         Route::middleware($apiMiddleware)->group(function () {
-            // Your API routes will be defined here, or you can load them from api.php
             require base_path('routes/api.php');
         });
     })
@@ -45,10 +36,3 @@ return Application::configure(basePath: dirname(__DIR__))
         // Configure exception handling if needed
     })
     ->create();
-=======
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
->>>>>>> e2a8b4e (Primer commit)
