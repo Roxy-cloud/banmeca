3d97a0290aabd08a29442dff2c9d8574c1cbc87e
Roxy-Cloud <rosanagarrido1972@gmail.com>
2025-04-18 11:15:59 -0400

    Corrigiendo conflictos de merge en Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,15 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-<<<<<<< HEAD
-=======
 import forms from '@tailwindcss/forms';
->>>>>>> e2a8b4e (Primer commit)
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
-<<<<<<< HEAD
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
     ],
-=======
-        './resources/views/**/*.blade.php',
-    ],
-
->>>>>>> e2a8b4e (Primer commit)
     theme: {
         extend: {
             fontFamily: {
@@ -26,10 +17,5 @@ export default {
             },
         },
     },
-<<<<<<< HEAD
-    plugins: [],
-=======
-
     plugins: [forms],
->>>>>>> e2a8b4e (Primer commit)
 };
